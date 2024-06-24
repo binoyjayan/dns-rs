ca10fd4f413fa76221e89410c4a2e1b66ca33e5a
Binoy Jayan <binoyjayan@gmail.com>
2024-06-24 07:06:05 -0700

    parse the header section

--- src/dns.rs
+++ src/dns.rs
@@ -1,3 +1,5 @@
+use std::convert::TryFrom;
+
 #[derive(Debug)]
 /// DNS Header
 /// 0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
@@ -25,6 +27,15 @@ pub(crate) struct DnsHeader {
 }
 
 impl DnsHeader {
+    pub(crate) const QR_MASK: u16 = 0b1000_0000_0000_0000;
+    pub(crate) const OPCODE_MASK: u16 = 0b0111_1000_0000_0000;
+    pub(crate) const AA_MASK: u16 = 0b0000_0100_0000_0000;
+    pub(crate) const TC_MASK: u16 = 0b0000_0010_0000_0000;
+    pub(crate) const RD_MASK: u16 = 0b0000_0001_0000_0000;
+    pub(crate) const RA_MASK: u16 = 0b0000_0000_1000_0000;
+    pub(crate) const Z_MASK: u16 = 0b0000_0000_0111_0000;
+    pub(crate) const RCODE_MASK: u16 = 0b0000_0000_0000_1111;
+
     pub(crate) fn new(id: u16) -> DnsHeader {
         DnsHeader {
             id,
@@ -35,37 +46,47 @@ impl DnsHeader {
             arcount: 0,
         }
     }
-    pub(crate) fn set_qr(&mut self, qr: bool) {
-        self.flags = (self.flags & !(1 << 15)) | ((qr as u16) << 15);
+    pub(crate) fn get_id(&self) -> u16 {
+        self.id
     }
 
-    #[allow(unused)]
+    pub(crate) fn get_opcode(&self) -> u8 {
+        ((self.flags & Self::OPCODE_MASK) >> 11) as u8
+    }
+    pub(crate) fn get_rd(&self) -> bool {
+        ((self.flags & Self::RD_MASK) >> 8) == 1
+    }
+
+    pub(crate) fn set_qr(&mut self, qr: bool) {
+        self.flags = (self.flags & !Self::QR_MASK) | ((qr as u16) << 15);
+    }
+
     pub(crate) fn set_opcode(&mut self, opcode: u8) {
-        self.flags = (self.flags & !(0b1111 << 11)) | ((opcode as u16) << 11);
+        self.flags = (self.flags & !Self::OPCODE_MASK) | ((opcode as u16) << 11);
     }
 
     pub(crate) fn set_aa(&mut self, aa: bool) {
-        self.flags = (self.flags & !(1 << 10)) | ((aa as u16) << 10);
+        self.flags = (self.flags & !Self::AA_MASK) | ((aa as u16) << 10);
     }
 
     pub(crate) fn set_tc(&mut self, tc: bool) {
-        self.flags = (self.flags & !(1 << 9)) | ((tc as u16) << 9);
+        self.flags = (self.flags & !Self::TC_MASK) | ((tc as u16) << 9);
     }
 
     pub(crate) fn set_rd(&mut self, rd: bool) {
-        self.flags = (self.flags & !(1 << 8)) | ((rd as u16) << 8);
+        self.flags = (self.flags & !Self::RD_MASK) | ((rd as u16) << 8);
     }
 
     pub(crate) fn set_ra(&mut self, ra: bool) {
-        self.flags = (self.flags & !(1 << 7)) | ((ra as u16) << 7);
+        self.flags = (self.flags & !Self::RA_MASK) | ((ra as u16) << 7);
     }
 
     pub(crate) fn set_z(&mut self, z: u8) {
-        self.flags = (self.flags & !(0b111 << 4)) | ((z as u16) << 4);
+        self.flags = (self.flags & !Self::Z_MASK) | ((z as u16) << 4);
     }
 
     pub(crate) fn set_rcode(&mut self, rcode: u8) {
-        self.flags = (self.flags & !(0b1111)) | rcode as u16;
+        self.flags = (self.flags & !Self::RCODE_MASK) | rcode as u16;
     }
 
     pub(crate) fn set_qdcount(&mut self, qdcount: u16) {
@@ -95,5 +116,20 @@ impl From<&DnsHeader> for Vec<u8> {
         bytes.extend_from_slice(&hdr.nscount.to_be_bytes());
         bytes.extend_from_slice(&hdr.arcount.to_be_bytes());
         bytes
+    }
+}
+
+impl<'a> TryFrom<&'a [u8]> for DnsHeader {
+    type Error = std::array::TryFromSliceError;
+
+    fn try_from(slice: &'a [u8]) -> Result<Self, Self::Error> {
+        Ok(Self {
+            id: u16::from_be_bytes(slice[0..2].try_into()?),
+            flags: u16::from_be_bytes(slice[2..4].try_into()?),
+            qdcount: u16::from_be_bytes(slice[4..6].try_into()?),
+            ancount: u16::from_be_bytes(slice[6..8].try_into()?),
+            nscount: u16::from_be_bytes(slice[8..10].try_into()?),
+            arcount: u16::from_be_bytes(slice[10..12].try_into()?),
+        })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,31 @@ use std::net::UdpSocket;
 
 mod dns;
 
-fn main() {
+fn convert_to_dns_header(buf: &[u8]) -> anyhow::Result<dns::DnsHeader> {
+    dns::DnsHeader::try_from(buf).map_err(|err| anyhow::anyhow!(err))
+}
+
+fn main() -> anyhow::Result<()> {
     let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
     let mut buf = [0; 512];
     loop {
         match udp_socket.recv_from(&mut buf) {
             Ok((size, source)) => {
-                let mut dns = dns::DnsHeader::new(1234);
+                let dnsreq = convert_to_dns_header(&buf)?;
+                let req_id = dnsreq.get_id();
+                let req_opcode = dnsreq.get_opcode();
+                let req_rd = dnsreq.get_rd();
+                println!("DNS request [ id: {}, opcode: {} ]", req_id, req_opcode);
+                let mut dns = dns::DnsHeader::new(req_id);
                 dns.set_qr(true);
+                dns.set_opcode(req_opcode);
                 dns.set_aa(false);
                 dns.set_tc(false);
-                dns.set_rd(false);
+                dns.set_rd(req_rd);
                 dns.set_ra(false);
                 dns.set_z(0);
-                dns.set_rcode(0);
+                // 0 if no error, 4 if not implemented
+                dns.set_rcode(4);
                 dns.set_qdcount(1);
                 dns.set_ancount(1);
                 dns.set_nscount(0);
@@ -47,4 +58,5 @@ fn main() {
             }
         }
     }
+    Ok(())
 }
