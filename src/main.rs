697e409180012904dd81433149b59fe37e2d8e9c
Binoy Jayan <binoyjayan@gmail.com>
2024-06-24 06:44:21 -0700

    write a dns answer section

--- src/main.rs
+++ src/main.rs
@@ -17,16 +17,24 @@ fn main() {
                 dns.set_z(0);
                 dns.set_rcode(0);
                 dns.set_qdcount(1);
-                dns.set_ancount(0);
+                dns.set_ancount(1);
                 dns.set_nscount(0);
                 dns.set_arcount(0);
 
                 let mut packet: Vec<u8> = Vec::from(&dns);
-                // Create and set the resource record
+                // DNS Query section
                 packet.extend_from_slice(b"\x0ccodecrafters\x02io\x00");
-                // DNS type A and class IN
-                packet.extend_from_slice(&1u16.to_be_bytes());
-                packet.extend_from_slice(&1u16.to_be_bytes());
+                packet.extend_from_slice(&1u16.to_be_bytes()); // DNS type A
+                packet.extend_from_slice(&1u16.to_be_bytes()); // class IN
+
+                // DNS Answer section
+                packet.extend_from_slice(b"\x0ccodecrafters\x02io\x00");
+                packet.extend_from_slice(&1u16.to_be_bytes()); // DNS type A
+                packet.extend_from_slice(&1u16.to_be_bytes()); // class IN
+                packet.extend_from_slice(&60u32.to_be_bytes()); // TTL
+                packet.extend_from_slice(&4u16.to_be_bytes()); // rdata length
+                packet.extend_from_slice(&[8, 8, 8, 8]); // rdata
+
                 let len = packet.len();
                 println!("Received {} bytes from {}, sending {}", size, source, len);
                 udp_socket
